941d674d33916970cc3d84a0668945421de9d4f0
Steve Emmanuel <steve.emmanuelck@gmail.com>
2022-07-11 13:56:57 +0530

    fixed imports

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,7 +1,7 @@
-from bank import Bank, BankLoan
-from borrower import Borrower
-from payment import Payment
-from ledger import Ledger, LedgerEntry
+from .bank import Bank, BankLoan
+from .borrower import Borrower
+from .payment import Payment
+from .ledger import Ledger, LedgerEntry
 
 __all__ = ('Bank', 'BankLoan', 'Borrower',
            'Payment', 'Ledger', 'LedgerEntry')
